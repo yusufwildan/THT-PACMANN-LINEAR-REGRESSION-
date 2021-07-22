be6c9a6b53f148e73a5a42e1b3b34c5474c7498c
Yusuf Wildan Agung Sanjaya <77679194+yusufwildan@users.noreply.github.com>
2021-07-22 09:20:01 +0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,58 @@
+class LinearReg:
+
+  #INPUT
+  def __init__(self):
+      self.data_x = []
+      self.data_y = []
+      self.coef = 0
+      self.intercept = 0
+      self.MSE = 0
+      self.Coefdet = 0
+
+  #LINEAR REGRESSION FUNCTION    
+  def fit(self, data_x, data_y):
+      self.data_x = data_x
+      self.data_y = data_y
+      
+      sumx=0;sumy=0;sumxy=0;sumx2=0
+
+      n = len(data_x)
+        
+      #sum of x
+      for i in range (0,n):
+          sumx = sumx + data_x[i]
+            
+      #sum of y
+      for i in range (0,n):
+          sumy = sumy + data_y[i]
+        
+      #sum of xy
+      for i in range (0,n):
+          sumxy = sumxy + data_x[i]*data_y[i]
+        
+      #sum of square of x
+      for i in range (0,n):
+          sumx2 = sumx2 + data_x[i]**2
+        
+      avgx = sumx/n # average of x
+      avgy = sumy/n # average of y
+        
+      m= (n*sumxy - sumx*sumy)/(n*sumx2 - sumx**2)
+      c= avgy - m*avgx
+        
+      st = 0; sr=0
+      for i in range (0,n):
+          st = st + (data_y[i]-avgy)**2
+          sr = sr + (data_y[i]-m*data_x[i]-c)**2
+        
+      MSE = (sr/n) # Mean squared error
+      Coefdet = (st-sr)/st # Coefficient of determination
+      
+      self.coef = m
+      self.intercept = c
+      self.MSE = MSE
+      self.Coefdet = Coefdet
+
+  #PREDICT DATA BASED ON REGRESSION FUNCTION ABOVE    
+  def predict(self, x):
+      return self.coef*x + self.intercept
